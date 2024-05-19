5fd94e251bd64c60230398e6b0b2331510df3146
hvoking <gus@ugeom.com>
2024-05-19 01:10:44 -0400

    changed summary

--- src/components/landing/pages/page3/about/architect/resume/pdf/summary/index.tsx
+++ src/components/landing/pages/page3/about/architect/resume/pdf/summary/index.tsx
@@ -4,14 +4,14 @@ import './styles.scss';
 export const Summary = () => {
 	return (
 		<p className="resume-summary">
-			Architect with a strong foundation in programming, 
+			Highly productive Computational Designer, 
 			specializing in seamlessly integrating web technologies 
 			into architectural and urban design. 
-			My work revolves around creating parametric web applications 
+			My work revolves around creating web applications 
 			with stunning graphical interfaces 
 			and precise geometric representations. 
 			These applications effectively convey information, 
-			with an emphasis on interactivity and geolocation features.
+			with an emphasis on interactivity and geolocation.
 		</p>
 	)
 }
